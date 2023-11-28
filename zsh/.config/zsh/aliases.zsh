945a6a8e9a4b70062629aaab353acd4872bea98d
McLOVIN <shortcut7777@gmail.com>
2023-11-28 09:03:14 +0800

    Update aliases.zsh

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,12 +1,9 @@
 #!/bin/sh
+alias reload="source ~/.zshrc"
 alias j='z'
 alias f='zi'
-alias g='lazygit'
+alias g='gitui'
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
-alias nvimrc='nvim ~/.config/nvim/'
-alias yay="paru"
-
-# alias lvim='nvim -u ~/.local/share/lunarvim/lvim/init.lua --cmd "set runtimepath+=~/.local/share/lunarvim/lvim"'
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
@@ -14,9 +11,6 @@ alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/p
 alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
 alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
 
-# Remarkable
-alias remarkable_ssh='ssh root@10.11.99.1'
-alias restream='restream -p'
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
@@ -41,39 +35,23 @@ alias pscpu='ps auxf | sort -nr -k 3 | head -5'
 # gpg encryption
 # verify signature for isos
 alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
-# receive the key of a developer
+a# receive the key of a developer
 alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
-
-# For when keys break
-alias archlinx-fix-keys="sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --refresh-keys"
-
-# systemd
-alias mach_list_systemctl="systemctl list-unit-files --state=enabled"
-
-alias mach_java_mode="export SDKMAN_DIR="$HOME/.sdkman" && [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh""
 
 alias m="git checkout master"
 alias s="git checkout stable"
 
-if [[ $TERM == "xterm-kitty" ]]; then
-  alias ssh="kitty +kitten ssh"
-fi
+#exa
+alias ll="eza -lbF --git"
+alias la="eza -lbhHigmuSa --time-style=long-iso --git --color-scale"
+alias lx="eza -lbhHigmuSa@ --time-style=long-iso --git --color-scale"
+alias llt="eza -l --git --tree"
+alias lt="eza --tree --level=2"
+alias llm="eza -lbGF --git --sort=modified"
+alias lld="eza -lbhHFGmuSa --group-directories-first"
 
-case "$(uname -s)" in
-
-Darwin)
-	# echo 'Mac OS X'
-	alias ls='ls -G'
-	;;
-
-Linux)
-	alias ls='ls --color=auto'
-	;;
-
-CYGWIN* | MINGW32* | MSYS* | MINGW*)
-	# echo 'MS Windows'
-	;;
-*)
-	# echo 'Other OS'
-	;;
-esac
+#tmux
+alias t="tmux"
+alias tn='_tmux_new() { t new -s "$1"; }; _tmux_new'
+alias ta='_tmux_attach() { t attach -t "$1"; }; _tmux_attach'
+alias tk='_tmux_kill() { t kill-sess -t "$1"; }; _tmux_kill'
